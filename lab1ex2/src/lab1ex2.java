391e3a26d5baf7056715df453ab93c8c3c37c05b
Alex <alex05.vo@gmail.com>
2024-03-21 13:20:44 +0200

    lab1ex2

---
+++ lab1ex2/src/lab1ex2.java
@@ -0,0 +1,17 @@
+public class lab1ex2 {
+    public static void main(String[] args) {
+        
+        String word1 = "Мене";
+        String word2 = "звати";
+        String word3 = "Олександр,";
+        String word4 = "я";
+        String word5 = "люблю";
+        String word6 = "пити";
+        String word7 = "чай";
+        String word8 = "та";
+        String word9 = "подорожувати";
+        String word10 = "Україною";
+
+        System.out.println(word1 + " " + word2 + " " + word3 + " " + word4 + " " + word5 + " " + word6 + " " + word7 + " " + word8 + " " + word9 + " " + word10);
+    }
+}
