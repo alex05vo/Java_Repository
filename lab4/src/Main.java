c739615df742d344e8261509e43f345bc684ede0
Alex <alex05.vo@gmail.com>
2024-04-24 20:38:29 +0300

    redacted

--- lab4/src/Main.java
+++ lab4/src/Main.java
@@ -12,7 +12,7 @@ public class Main {
             userInput = scanner.nextLine();
 
             if (userInput.equalsIgnoreCase("q")) {
-                break;
+                return;
             }
 
             switch (userInput) {
@@ -20,25 +20,25 @@ public class Main {
                     System.out.println("-------------------------------");
                     Task_1 task1 = new Task_1();
                     task1.Array50();
-                    break;
+                    return;
                 case "2":
                     System.out.println("-------------------------------");
                     Task_2 task2 = new Task_2();
                     task2.Array10();
-                    break;
+                    return;
                 case "3":
                     System.out.println("-------------------------------");
                     Task_3 task3 = new Task_3();
                     task3.ArraySum();
-                    break;
+                    return;
                 case "4":
                     System.out.println("-------------------------------");
                     Task_4 task4 = new Task_4();
                     task4.Change();
-                    break;
+                    return;
                 default:
                     System.out.println("Неправильний ввiд. Введiть номер завдання (1; 2; 3; 4) або 'q' для виходу.");
-                    break;
+                    return;
             }
         }
     }
