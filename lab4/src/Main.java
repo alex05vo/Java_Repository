124cd66546d04b4845f3eb990cc136aea1a1fc9b
Alex <alex05.vo@gmail.com>
2024-04-29 01:05:25 +0300

    тепер працює

--- lab4/src/Main.java
+++ lab4/src/Main.java
@@ -20,25 +20,25 @@ public class Main {
                     System.out.println("-------------------------------");
                     Task_1 task1 = new Task_1();
                     task1.Array50();
-                    return;
+                    break;
                 case "2":
                     System.out.println("-------------------------------");
                     Task_2 task2 = new Task_2();
                     task2.Array10();
-                    return;
+                    break;
                 case "3":
                     System.out.println("-------------------------------");
                     Task_3 task3 = new Task_3();
                     task3.ArraySum();
-                    return;
+                    break;
                 case "4":
                     System.out.println("-------------------------------");
                     Task_4 task4 = new Task_4();
                     task4.Change();
-                    return;
+                    break;
                 default:
                     System.out.println("Неправильний ввiд. Введiть номер завдання (1; 2; 3; 4) або 'q' для виходу.");
-                    return;
+                    break;
             }
         }
     }
